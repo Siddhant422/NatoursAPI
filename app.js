f7b3e11fc87efde5c2dabad08f91842e6faae1b4
Siddhant Keshari <siddhantkeshari76@gmail.com>
2024-01-01 02:53:09 +0400

    Day 3 Commit

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const morgan = require('morgan');
 //Now app will be able to use epxress methods;
 const app = express();
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 //1) MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
 // this is used to access the static files in the directory
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
