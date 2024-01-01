a67d5af2bb142fd28bf9695c05411417092a2f62
Siddhant Keshari <siddhantkeshari76@gmail.com>
2024-01-02 02:39:17 +0400

    Day 5 Commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,87 +1,54 @@
-const fs = require('fs');
+// eslint-disable-next-line no-unused-vars
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour Id: ${val}`)
-  if (val * 1 > tours.length) {
-    return res.status(404).json({
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
       status: 'Failed',
-      message: 'Invalid ID',
+      message: 'Missing Name or Price',
     });
   }
   next();
 };
 
-exports.checkBody = (req,res,next) => {
-    if(!req.body.name || !req.body.price) {
-      return res.status(400).json({
-        status: 'Failed',
-        message: 'Missing Name or Price'
-      })
-    }
-    next();
-}
-
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  //Object.assign(Target, Source) -> Assign req.body to this id
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'Success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'Success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'Success',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   //This is a trick and it will convert the string to a integer
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'Failed',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      tour,
-    },
-  });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // if (!tour) {
+  //   return res.status(404).json({
+  //     status: 'Failed',
+  //     message: 'Invalid ID',
+  //   });
+  // }
+  // res.status(200).json({
+  //   status: 'Success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'Failed',
-      message: 'Invalid ID',
-    });
-  }
   res.status(200).json({
     status: 'Success',
     data: {
