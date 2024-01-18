77f3f6fa2b79134670946160b97213ab379cb319
Siddhant Keshari <siddhantkeshari76@gmail.com>
2024-01-19 03:31:34 +0400

    Day 14 Of BackEnd

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const app = require('./app')
+const app = require('./app');
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exceptions');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 //Here we have created a port
 const port = 3000;
 
 //Listen function is like a switch that turn on the server for req and res
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}..`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
